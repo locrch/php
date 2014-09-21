41c636bc6e36e86cab6bf7c5d7d5d86e9008a0a8
yuhaoqian <402290527@qq.com>
2014-09-21 16:07:23 +0800

    2014年9月21日16:07:12 phpleran

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yu
+ * Date: 14-9-21
+ * Time: 下午1:23
+ */
+$ag="a";
+$fd=false;
+echo $ag;
+
+$ac = <<< eaf
+fdfsf dfd fdg
+dfsdg
+eaf;
+
+$ad = array("fd","fs",false,123);
