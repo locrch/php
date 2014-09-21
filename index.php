d5562b0fec9855677aa22ec2ef148dd1b1c12510
fish_ball <alfred.h@163.com>
2014-09-21 16:18:49 +0800

    fish-ball is updating your repo!
    about to pull
    
    Signed-off-by: fish_ball <alfred.h@163.com>

--- index.php
+++ index.php
@@ -5,14 +5,5 @@
  * Date: 14-9-21
  * Time: 下午1:23
  */
-$ag="a";
-$fd=false;
-echo $ag;
-
-$ac = <<< eaf
-fdfsf dfd fdg
-dfsdg
-eaf;
-
-$ad = array("fd","fs",false,123);
-$df = "fd";
+// fish-ball is updating your repo!
+echo 'Hello Yu!';
